b494288e54a95000741b9e1f7a4544c2fa50c728
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2014-07-14 03:40:49 +0200

    remove Vagrantfile

--- changeconf.sh
+++ changeconf.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+#  little helper for changing configs. will search for $1 in user config
+#+ files. in case $2 is stated, $1 will be changed to $2 in all files.
+#+ Use this for changing hostname, etc.
 
 if [ $# -eq 0 ] ; then
   echo $#
@@ -11,7 +15,8 @@ echo
 echo "Here are the occurences of <${1}>:"
 echo
 
-grep -ri  --exclude "*example" --exclude "*orig" ${1} user-config
+grep -ri  --exclude "*example" --exclude "*orig" ${1} \
+  user-config Vagrantfile
 
 if [ -n "${2}" ] ; then
 
@@ -21,7 +26,8 @@ if [ -n "${2}" ] ; then
   echo "Press Ctrl-C to quit or RETURN to continue."
   read nil
   
-  grep -ril --exclude "*example" --exclude "*orig" ${1} user-config | xargs sed -i.orig -e "s/${1}/${2}/g"
+  grep -ril --exclude "*example" --exclude "*orig" ${1} \
+    user-config Vagrantfile | xargs sed -i.orig -e "s/${1}/${2}/g"
 fi
 
 exit 0
