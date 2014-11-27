c75d6fcc24a0f25450bd3b732e2cf01f0715a96e
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2014-11-27 10:41:25 +0100

    hostenv iam2 added

---
+++ user-iam2/hostenv/env/acc.sh
@@ -0,0 +1,25 @@
+export   ORACLE_BASE=/appl/iam/fmw
+export       MW_HOME=${ORACLE_BASE}/products/access
+export       WL_HOME=${MW_HOME}/wlserver_10.3
+export   ORACLE_HOME=${MW_HOME}/iam
+export ORACLE_COMMON=${MW_HOME}/oracle_common
+export     JAVA_HOME=${MW_HOME}/jdk/current
+
+export        DOMAIN=IAMAccessDomain
+export   DOMAIN_HOME=/appl/iam/fmw/config/domains/${DOMAIN}
+export    DOMAIN_LOG=/appl/logs/iam/IAMAccessDomain
+unset       INSTANCE
+unset      INST_HOME
+unset       INST_LOG
+
+export          PATH=${ORACLE_HOME}/bin:${JAVA_HOME}/bin:/appl/iam/ruby/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/iam/bin
+export             E=acc
+
+alias wlst='${ORACLE_COMMON}/common/bin/wlst.sh -loadProperties ${HOME}/.env/access.prop'
+
+deploy() {
+  . ${WL_HOME}/server/bin/setWLSEnv.sh
+  java weblogic.WLST -loadProperties ~/.wlst/${1}.prop ${HOME}/lib/deploy.py
+}
+
+
---
+++ user-iam2/hostenv/env/idm.sh
@@ -0,0 +1,24 @@
+export   ORACLE_BASE=/appl/iam/fmw
+export       MW_HOME=${ORACLE_BASE}/products/identity
+export       WL_HOME=${MW_HOME}/wlserver_10.3
+export   ORACLE_HOME=${MW_HOME}/iam
+export ORACLE_COMMON=${MW_HOME}/oracle_common
+export     JAVA_HOME=${MW_HOME}/jdk/current
+
+export        DOMAIN=IAMGovernanceDomain
+export   DOMAIN_HOME=/appl/iam/fmw/config/domains/${DOMAIN}
+export    DOMAIN_LOG=/appl/logs/iam/IAMGovernanceDomain
+unset       INSTANCE
+unset      INST_HOME
+unset       INST_LOG
+
+export          PATH=${ORACLE_HOME}/bin:${JAVA_HOME}/bin:/appl/iam/ruby/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/iam/bin
+export             E=idm
+
+alias wlst='${ORACLE_COMMON}/common/bin/wlst.sh -loadProperties ${HOME}/.env/identity.prop'
+
+deploy() {
+  . ${WL_HOME}/server/bin/setWLSEnv.sh
+  java weblogic.WLST -loadProperties ~/.wlst/${1}.prop ${HOME}/lib/deploy.py
+}
+
