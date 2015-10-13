bb7d9f6bdbe5b189f841b060ee151e8f96817157
Velimir Iveljic <velimir.iveljic@agoracon.at>
2015-10-13 15:00:54 +0200

    separated wls and domain functions

--- lib/libpatchwls.sh
+++ lib/libpatchwls.sh
@@ -4,40 +4,52 @@
 #   * jvm options are not jdk7 compatible
 #
 
-patch_wls_domain()
+patch_wls_bin()
 {
   if [ "${1}" == "" ] ; then
     error "ERROR: Parameter missing"
     exit 0
   fi
   
-  local src=${1}
-  
+  local _p=${1}
+  local _src=${DEPLOYER}/lib/weblogic
+
   # wl_home
-  if grep -e 'PRODUCTION_MODE="true"' ${WL_HOME}/common/bin/commEnv.sh >/dev/null ; then
+  if grep -e 'PRODUCTION_MODE="true"' ${iam_top}/${_p}/wlserver_10.3/common/bin/commEnv.sh >/dev/null ; then
     log "WL_HOME already patch, nothing to do"
   else
-    patch -b ${WL_HOME}/common/bin/commEnv.sh <${src}/wl_home/commEnv.sh.patch
-    log "WL_HOME patched: ${WL_HOME}"
+    patch -b ${iam_top}/${_p}/common/bin/commEnv.sh <${_src}/weblogic/commEnv.sh.patch
+    log "WL_HOME patched: ${iam_top}/${_p}"
   fi
-  
+}
+
+patch_wls_domain()
+{
+  if [ "${1}" == "" ] ; then
+    error "ERROR: Parameter missing"
+    exit 0
+  fi
+
+  local _domain=${1}
+  local _src=${DEPLOYER}/lib/${_domain}
+
   # admin domain
   if ! [ -a ${ADMIN_HOME}/bin/setCustDomainEnv.sh ]; then
-    cp ${src}/domain/setCustDomainEnv.sh ${ADMIN_HOME}/bin/
+    cp ${_src}/domain/setCustDomainEnv.sh ${ADMIN_HOME}/bin/
   fi
-  
+
   if ! grep setCustDomainEnv ${ADMIN_HOME}/bin/setDomainEnv.sh >/dev/null 2>&1 ; then
-    patch -b ${ADMIN_HOME}/bin/setDomainEnv.sh <${src}/domain/setDomainEnv.sh.patch
+    patch -b ${ADMIN_HOME}/bin/setDomainEnv.sh <${_src}//domain/setDomainEnv.sh.patch
     log "ADMIN_HOME patched: ${ADMIN_HOME}"
   fi
-  
+
   # local domain
   if ! [ -a ${WRK_HOME}/bin/setCustDomainEnv.sh ]; then
-    cp ${src}/domain/setCustDomainEnv.sh ${WRK_HOME}/bin/
+    cp ${_src}/domain/setCustDomainEnv.sh ${WRK_HOME}/bin/
   fi
-  
+
   if ! grep setCustDomainEnv ${WRK_HOME}/bin/setDomainEnv.sh >/dev/null 2>&1 ; then
-    patch -b ${WRK_HOME}/bin/setDomainEnv.sh <${src}/domain/setDomainEnv.sh.patch
+    patch -b ${WRK_HOME}/bin/setDomainEnv.sh <${_src}/domain/setDomainEnv.sh.patch
     log "WRK_HOME patched: ${WRK_HOME}"
   fi
 }
