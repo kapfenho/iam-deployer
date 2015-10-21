ef47719d1b8b7c13b8f194c80162430e29ed8385
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2015-10-22 01:21:56 +0200

    removed obsolete files, start-all, stop-all

--- lib/templates/iam/env-iam2/idm-deplenv.sh
+++
@@ -1,9 +0,0 @@
-# set deployment environment
-#
-. ${HOME}/.env/common.sh
-. ${HOME}/.env/idm.sh
-. ${WL_HOME}/server/bin/setWLSEnv.sh
-deploy() {
-  java weblogic.WLST -loadProperties ~/.wlst/${1}.prop ${HOME}/lib/deploy.py
-}
-
