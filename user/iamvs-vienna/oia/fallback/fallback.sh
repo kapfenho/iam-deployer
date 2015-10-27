fd0e0d1bbb1006aee00fce29ce25875810dc4eaa
Velimir Iveljic <velimir.iveljic@agoracon.at>
2015-10-27 17:39:33 +0100

    adding oia files to IAMVS instance

---
+++ user/iamvs-vienna/oia/fallback/fallback.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# create OIA Database User and Schema
+
+set -o errexit nounset
+
+iam_oia_prefix=IAM1I_OIA
+iam_oia_schema_pass=Montag11
+
+#DEPLOYER=/home/fmwuser/iam-deployer
+SQLPLUS='sudo -i -u oracle sqlplus'
+
+. ${DEPLOYER}/user-config/iam.config
+
+echo "Creating OIA Database User and Schema.."
+
+# create OIA user
+${SQLPLUS} -s / as sysdba &> /tmp/oia_schema.log << EOF
+@${s_oia}/sql/00-create-tablespace.sql
+@${s_oia}/sql/01-create-user.sql
+commit;
+exit;
+EOF
+
+${SQLPLUS} -s ${iam_oia_prefix}/${iam_oia_schema_pass}@${dbs_dbhost}/${iam_sid} &> /tmp/oia_schema.log << EOF
+@${s_oia}/sql/10-create-schema.sql
+@${s_oia}/sql/20-mig1.sql
+@${s_oia}/sql/21-mig2.sql
+@${s_oia}/sql/22-mig3.sql
+commit;
+exit;
+EOF
+
+echo "Created OIA Database User and Schema."
