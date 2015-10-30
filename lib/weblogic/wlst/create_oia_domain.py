b51aefcdbeebbc578a26e5c5c6ac344c1e10ad9e
Velimir Iveljic <velimir.iveljic@agoracon.at>
2015-10-30 12:50:07 +0100

    enroll multiple Nodemanagers

--- lib/weblogic/wlst/create_oia_domain.py
+++ lib/weblogic/wlst/create_oia_domain.py
@@ -4,16 +4,26 @@ createDomain=os.path.dirname(sys.argv[0]) +'/heinz/createDomain.py'
 if os.path.exists(createDomain):
   execfile(createDomain)
 
-
+# update Nodemanager credentials for new domain
 def updateNmUser():
     print "Updating Nodemanager Username and password"
     edit()
     startEdit()
+    cd("/SecurityConfiguration/" + domainProps.getProperty('domainName'))
     cmo.setNodeManagerUsername("admin")
-    cmo.setNodeManagerPassword(AdminPassword)
+    cmo.setNodeManagerPassword(adminPassword)
     save()
     activate()
 
+# do enroll on local machine
+def enrollNm():
+    nrMachines = domainProps.getProperty('amountMachine')
+
+    machine = 1
+    while (machine <= int(nrMachines)):
+        nmDir = get_instance_property('machine', str(machine), 'nmDir')
+        print ' Do enroll '+ domainLocation +'  -  '+ nmDir +' !\n'
+        nmEnroll(domainLocation, nmDir)
 
 # ================================================================
 #           Main Code Execution
@@ -29,8 +39,6 @@ if __name__== "main":
     createAllBootProperties()
     startAndConnnectToAdminServer()
     updateNmUser()
-    # do enroll on local machine
-    print ' Do enroll '+ domainLocation +'  -  '+ domainProps.getProperty('nmDir')+' !\n'
-    nmEnroll(domainLocation, domainProps.getProperty('nmDir'))
+    enrollNm()
     setJTATimeout()
     createAllDatasources()
