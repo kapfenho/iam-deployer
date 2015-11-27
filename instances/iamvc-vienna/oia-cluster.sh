a204cb2137fdff94b4b532ae7e8f8261abefd17b
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2015-11-27 16:34:32 +0100

    iamvc cluster config

--- instances/iamvc-vienna/oia-cluster.sh
+++ instances/iamvc-vienna/oia-cluster.sh
@@ -5,7 +5,13 @@
 set -o errexit
 set -o errtrace
 
-srvoia2=oim2
+oim1=oim1
+oim2=oim2
+oia1=oim1
+oia2=oim2
+web1=web1
+web2=web2
+
 export PATH=${DEPLOYER}:${PATH}
 
 iam rcu -a create -t analytics
@@ -18,8 +24,10 @@ iam weblogic -a install -t analytics
 
 # since products/analytics is avail: new env
 # cluster: both have -A for all hosts
-iam userenv -a env -A
-iam userenv -a profile -A
+iam userenv -a env -H $oia1
+iam userenv -a env -H $oia2
+iam userenv -a profile -H $oia1
+iam userenv -a profile -H $oia1
 
 # copy lib files
 iam weblogic -a wlstlibs -t analytics
@@ -27,7 +35,7 @@ iam weblogic -a wlstlibs -t analytics
 # create cluster domain and distribute
 iam analytics -a domcreate -P cluster
 iam analytics -a rdeploy -P pack
-iam analytics -a rdeploy -P unpack -H $srvoia2
+iam analytics -a rdeploy -P unpack -H $oia2
 
 # unpack OIA application
 iam analytics -a explode
@@ -36,8 +44,7 @@ iam analytics -a explode
 iam analytics -a domconfig
 
 # configure OIA application instance
-# TODO
-iam analytics -a appconfig -P single
+iam analytics -a appconfig -P cluster
 
 # perform OIM-OIA integration steps
 iam analytics -a oimintegrate
