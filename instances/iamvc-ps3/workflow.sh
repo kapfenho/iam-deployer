64f56384962ec3109e40ce0835d0391c105401b9
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2016-02-19 16:44:17 +0100

    workflow changes for cluster

--- instances/iamvc-ps3/workflow.sh
+++ instances/iamvc-ps3/workflow.sh
@@ -2,11 +2,8 @@
 #
 #  IAM Setup workflow for cluster
 #
-#  Prerequisites:
-#  - there should be ssh-key communication available
-#    on hosts on which the deployment should be performed
-# 
-set -o errexit errtrace
+set -o errexit
+set -o errtrace
 
 export DEPLOYER
 export PATH=${DEPLOYER}:${PATH}
@@ -43,13 +40,13 @@ do
   iam lcmstep -a ${step} -A
 done
 
-# deploy user environment in shared location
-iam userenv -a env -A
+# ---- post install phase 1 ---
+#
+# all services need to be up
 
-# on each host: load in user profile and create easy to reach shortcuts 
+iam userenv -a env -A
 iam userenv -a profile -A
 
-# copy weblogic libraries
 iam weblogic -a wlstlibs -t identity -H ${oim1}
 iam weblogic -a wlstlibs -t access   -H ${oam1}
 
@@ -66,9 +63,9 @@ iam access   -a keyfile -u ${domaUser} -p ${domaPwd} -H ${oam1}
 iam access   -a keyfile -u ${domaUser} -p ${domaPwd} -H ${oam2}
 
 iam identity -a config -H ${oim1}
-
 iam access   -a config -H ${oam1}
 
+# stop all services - end of postinstall phase 1
 ssh ${oim1} -- $SHELL -l ~/bin/stop-identity
 ssh ${oam1} -- $SHELL -l ~/bin/stop-access
 ssh ${oim1} -- $SHELL -l ~/bin/stop-nodemanager
@@ -80,20 +77,29 @@ ssh ${oud2} -- $SHELL -l ~/bin/stop-dir
 ssh ${web1} -- $SHELL -l ~/bin/stop-webtier
 ssh ${web2} -- $SHELL -l ~/bin/stop-webtier
 
-exit 0
+# ---- post install phase 2 ---
+#
+# all services need to be down
 
-iam weblogic -a jdk7fix -t identity -H ${oim1}
-iam identity -a jdk7fix -H ${oim1}
-iam identity -a jdk7fix -H ${oim2}
+iam weblogic  -a jdk7fix -t identity -H ${oim1}
+iam weblogic  -a jdk7fix -t access   -H ${oam1}
 
-# identity domain post-install steps
-iam identity -a movelogs -H ${oim1}
-iam identity -a movelogs -H ${oim2}
+iam identity  -a domainfix -H ${oim1}
+iam identity  -a domainfix -H ${oim2}
 
-# webgate installation bug fix
-iam webtier -a movelogs -H ${web1}
-iam webtier -a movelogs -H ${web2}
-iam webtier -a config -v -H ${web1}
-iam webtier -a config -v -H ${web2}
+iam access    -a domainfix -H ${oam1}
+iam accss     -a domainfix -H ${oam2}
+
+iam identity  -a movelogs  -H ${oim1}
+iam identity  -a movelogs  -H ${oim2}
+iam access    -a movelogs  -H ${oam1}
+iam access    -a movelogs  -H ${oam2}
+iam directory -a movelogs  -H ${oud1}
+iam directory -a movelogs  -H ${oud2}
+iam webtier   -a movelogs  -H ${web1}
+iam webtier   -a movelogs  -H ${web2}
+
+iam webtier   -a config    -H ${web1}
+iam webtier   -a config    -H ${web2}
 
 echo -e "\nSetup finished successfully!\n"
