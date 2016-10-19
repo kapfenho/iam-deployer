3d00793a9893a3be2f86e2d380dafd702b0d0514
Horst Kapfenberger <horst.kapfenberger@agoracon.at>
2016-10-19 15:35:17 +0200

    web: adding custom dwpbank stuff

---
+++ lib/weblogic/wlst/enable_dcc.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env wlst
+
+# acConnect()
+
+editWebgate11gAgent(agentName="Webgate_IDM_11g",
+        allowManagementOperation="true",
+        allowCredentialCollectorOperations="true")
+
